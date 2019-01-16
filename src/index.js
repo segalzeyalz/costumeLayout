aef530fbc9d1589a8da08cb29a9d7e3d2f7f3d89
segalzeyalz <segaleseo@gmail.com>
2019-01-16 12:23:49 +0200

    add creatstore

--- src/index.js
+++ src/index.js
@@ -6,7 +6,12 @@ import reducers from './reducers';
 import App from './components/App';
 import * as serviceWorker  from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers);
+
+ReactDOM.render(
+    <Provider store={store} >
+        <App />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
