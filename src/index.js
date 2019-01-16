b8348670b1ec70f17fbc79facee809f875e4cb7c
segalzeyalz <segaleseo@gmail.com>
2019-01-16 12:20:03 +0200

    adding react grid

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import {createStore} from 'redux';
-import reducers from './reducers'
+import { createStore } from 'redux';
+import reducers from './reducers';
 import App from './components/App';
 import * as serviceWorker  from './serviceWorker';
 
