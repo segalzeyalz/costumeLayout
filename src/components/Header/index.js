f9a404636bce20b0689c2fc577f25fe69865558c
segalzeyalz <segaleseo@gmail.com>
2019-01-17 15:14:08 +0200

    add gradient background

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CSS from './Header.css';
+
+const Header = () => {
+  return <div className={CSS.Header}>
+            <h1 className={CSS.title}>
+              Costume Layout
+            </h1>
+          </div>
+};
+export default Header;
