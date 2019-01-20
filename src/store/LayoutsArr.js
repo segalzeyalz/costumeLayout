e5ee13ef52351d821191fe48d9c7a928dd7fbdf4
segalzeyalz <segaleseo@gmail.com>
2019-01-20 07:39:33 +0200

    add more layoputs

--- src/store/LayoutsArr.js
+++ src/store/LayoutsArr.js
@@ -22,7 +22,27 @@ const layoutsArr = [
               {i: 'g', x: 6, y: 2, w: 2, h: 2, static: true},
               {i: 'h', x: 8, y: 2, w: 1, h: 3, static: true},
               {i: 'i', x: 1, y: 4, w: 7, h: 1, static: true},
-            ]}
+            ]},
+            { id:3,
+              gridStructure: [
+                {i: 'a', x: 0, y: 0, w: 8, h: 3, static: true},
+                {i: 'b', x: 8, y: 0, w: 1, h: 4, static: true},
+                {i: 'c', x: 0, y: 3, w: 8, h: 1, static: true},
+                {i: 'd', x: 0, y: 4, w: 9, h: 1, static: true},
+                  ]},
+            { id:4,
+              gridStructure: [
+                    {i: 'a', x: 0, y: 0, w: 6, h: 2, static: true},
+                    {i: 'b', x: 6, y: 0, w: 3, h: 4, static: true},
+                    {i: 'c', x: 0, y: 2, w: 6, h: 2, static: true},
+                    {i: 'd', x: 0, y: 4, w: 9, h: 1, static: true},
+                ]},
+                { id:5,
+                gridStructure: [
+                        {i: 'a', x: 0, y: 0, w: 6, h: 2, static: true},
+                        {i: 'b', x: 6, y: 0, w: 3, h: 4, static: true},
+                        {i: 'c', x: 0, y: 2, w: 6, h: 2, static: true},
+                    ]}
 ];
 
 export default {layoutsArr};
