eb30865099ba24ed6c0cc7aed4e0376a424bc3de
segalzeyalz <segaleseo@gmail.com>
2019-01-22 08:45:21 +0200

    make a costume layout

---
+++ src/reducers/compElementsReducer.js
@@ -0,0 +1,16 @@
+import * as actionTypes from './../constants/actionTypes';
+
+const initialState = {
+    
+};
+
+const reducer = (state = initialState, action) => {
+    let {layOuts} = state;
+    switch (action.type) {
+       
+        
+    }
+    return state;
+};
+
+export default reducer;
